2c58ceee44e81dded4866c880f73f16936a99019
Louis Eiermann <eiermann@mothership.de>
2021-11-05 10:31:20 +0100

    build mapping issue fix

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use('/build',express.static(path.join(__dirname, '../build')));
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../build/', "index.html"));
 });
