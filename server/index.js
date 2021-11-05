abb7e2c17ccf14e1e1f03a610127abd3087d2b22
Louis Eiermann <eiermann@mothership.de>
2021-11-05 11:30:37 +0100

    API endpoint domain change update .eu/.com first basic auth implementation

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const path = require('path');
+const basicAuth = require('express-basic-auth')
+
 const app = express();
 
 app.use('/build',express.static(path.join(__dirname, '../build')));
+
+app.use(basicAuth({
+    challenge: true,
+    users: { 'admin': 'supersecret' }
+}))
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
